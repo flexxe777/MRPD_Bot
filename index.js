41e253acf09973973bd6de443fde3e23683d7283
flexxe777 <metehanmetin0430@gmail.com>
2026-07-22 17:21:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -859,7 +859,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
        } catch (error) {
         console.error('Mülakat bildirim sistemi hatası:', error);
     }
-  } // <-- 833. satırdaki if bloğunun eksik kalan kapanışı
-}); // <-- client.on event'ini ve async fonksiyonunu kapatan parantez
+} // <-- 833. satırdaki if bloğunun kapanışı
+}); // <-- client.on event'inin kapanışı
 
 client.login(TOKEN);
